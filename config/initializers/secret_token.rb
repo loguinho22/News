f5606ead0abc78113ac801127d936d093a25662e
Fernando Andrade <log22@mai.l.to>
2011-10-30 23:34:33 -0200

    Primeira Atualização

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Blog::Application.config.secret_token = '0d46902a1adc34a714860efbcadd61eac5964279a23f52c99993515b19b05419eb6e20e4bdf0e60ba3ccace749f92542a11dc5312819f50ec8126f0bf0b5a4d8'
---
+++ db/migrate/20111031013143_create_news.rb
@@ -0,0 +1,18 @@
+class CreateNews < ActiveRecord::Migration
+  def self.up
+    create_table :news do |t|
+
+    
+      t.string :titulo
+      t.string :autor
+      t.text :texto
+      
+      
+     t.timestamps
+    end
+  end
+
+  def self.down
+    drop_table :news
+  end
+end
